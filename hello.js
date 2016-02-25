67976b8611c4d3447dc1823ca4382af1644d1dd1
Valentin Gosu <valentin.gosu@gmail.com>
2016-02-25 04:19:16 +0100

    Push slides and controller

--- hello.js
+++ hello.js
@@ -1,13 +1,46 @@
 'use strict';
 
 if ('serviceWorker' in navigator) {
-  // The scope is by default '.'
-  // Trying to register a service worker for / or a parent dir will fail
-  navigator.serviceWorker.register('serviceWorker.js', { scope: '.'}).then(function(registration) {
-    // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
-  }).catch(function(err) {
+
+  let register = navigator.serviceWorker.register('serviceWorker.js');
+  register.catch(function(err) {
     // registration failed :(
     console.log('ServiceWorker registration failed: ', err);
+  });
+  let subscription = register.then(function(registration) {
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+
+    return registration.pushManager.getSubscription()
+    .then(function(subscription) {
+      // If a subscription was found, return it.
+      if (subscription) {
+        return subscription;
+      }
+
+      // Otherwise, subscribe the user
+    return registration.pushManager.subscribe({ userVisibleOnly: false });
+    });
+  }).then(function(subscription) {
+    // This is the URL of the endpoint we need to call to get a notification
+    console.log(subscription.endpoint);
+
+    var rawKey = subscription.getKey ? subscription.getKey('p256dh') : '';
+    var key = rawKey ?
+        btoa(String.fromCharCode.apply(null, new Uint8Array(rawKey))) :
+        '';
+    console.log('key: ', key);
+
+    document.getElementById('sw_controller_link').href = "sendNotification.html?endpoint="+encodeURIComponent(subscription.endpoint)+"&key="+encodeURIComponent(key);
+  });
+
+  subscription.catch(function(err) {
+    console.log('ServiceWorker subscription failed: ', err);
+  });
+
+  navigator.serviceWorker.addEventListener('message', function(e) {
+      console.log("sw message", e.data);
+
+      var el = document.getElementsByClassName(e.data)[0];
+      el.click();
   });
 }
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,9 +1,11 @@
 'use strict';
 
+var version = "one";
+
 self.addEventListener('install', event => {
-  console.log('service worker - install');
+  console.log('service worker - install | version:', version);
   function onInstall () {
-    return caches.open('static')
+    return caches.open(version)
       .then(cache =>
         cache.addAll([
           'hello.js',
@@ -12,9 +14,59 @@ self.addEventListener('install', event => {
       );
   }
 
-  event.waitUntil(onInstall(event));
+  // Skip waiting. Activate immediately.
+  event.waitUntil(
+    onInstall()
+     .then( () => self.skipWaiting() )
+  );
 });
 
 self.addEventListener('activate', event => {
-  console.log('service worker - activate');
+  console.log('service worker - activate | version:', version);
+
+  function onActivate() {
+    return caches.keys().then(cacheKeys => {
+      var oldCacheKeys = cacheKeys.filter(key =>
+        key.indexOf(version) !== 0
+      );
+      var deletePromises = oldCacheKeys.map(oldKey => caches.delete(oldKey));
+      return Promise.all(deletePromises);
+    });
+  }
+
+  event.waitUntil(
+    onActivate()
+     .then( () => self.clients.claim() ) // This makes the SW take effect immediately
+  );
+});
+
+self.addEventListener('fetch', event => {
+  var request            = event.request;
+  var url                = new URL(request.url);
+  console.log('service worker - fetch', url.href);
+});
+
+self.addEventListener('push', function(event) {
+  var payload = event.data ? event.data.text() : 'Hello world';
+  console.log('service worker - push', payload);
+
+  if (payload.indexOf('notify:') == 0) {
+    payload = payload.slice(7);
+    event.waitUntil(
+      // There are many other possible options, for an exhaustive list see the specs:
+      //   https://notifications.spec.whatwg.org/
+      self.registration.showNotification('PragueJS Service workers', {
+        body: payload,
+        icon: 'logo.png',
+        vibrate: [500, 100, 500],
+      })
+    );
+  } else {
+    self.clients.matchAll().then(function(clients) {
+      clients.forEach(function(client) {
+        console.log(client);
+        client.postMessage(payload);
+      });
+    });
+  }
 });
